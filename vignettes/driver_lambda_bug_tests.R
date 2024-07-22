36759cb1e770fbf5b43ae8ecebe96252944d4e17
Daniel Ramirez <55102974+dan-ramirez-23@users.noreply.github.com>
2024-07-22 13:45:18 -0400

    bug fix tests

--- vignettes/driver_lambda_bug_tests.R
+++ vignettes/driver_lambda_bug_tests.R
@@ -4,39 +4,56 @@ devtools::install(args="--verbose")
 library(sRACIPE)#, lib.loc = "/Users/danramirez/localR/4.2.2-arm")
 
 # Test on repressilator
-topo <- data.frame(Source=c("A"), Target=c("B"), Type=c(2))
-nGenes <- 2
-nEdges <- 1
-genes <- c("A","B")
+topo <- data.frame(Source=c("A","A","B","C","D","D"), 
+                   Target=c("B","C","A","D","B","C"), 
+                   Type=c(2,1,2,2,1,2))
+genes <- unique(c(topo$Source, topo$Target))
+nGenes <- 4
+nEdges <- 6
 
 
 ## Simulation Parameters
-noise <- 0.2
+noise <- 0
 printTime <- 0.2
-stepSize <- 0.01
+stepSize <- 0.05
 simTime <- 100
 
 # initial conditions
 ics <- rep(50, nGenes)
 
 
-## Model Parameters
-g <- 30
-k <- 0.5
-n <- 4
-fc <- 50
-th <- 20
-
-
 
 ##### SIMULATION #####
-racipe <- sracipeSimulate(topo, numModels = 100, plots = FALSE, genIC = TRUE,
+racipe <- sracipeSimulate(topo, numModels = 10000, plots = FALSE, genIC = TRUE,
                               genParams = TRUE, integrate = TRUE, integrateStepSize = stepSize,
-                              simulationTime = 20, initialNoise = noise, nNoise = 1, simDet = F, anneal = F)
+                              simulationTime = 20, initialNoise = noise, nNoise = 0, simDet = T, anneal = F)
 
 
 # Save results
-saveRDS(racipe, file.path("tests","racipe_CTS_lambda_old.Rds"))
+#saveRDS(racipe, file.path("tests","racipe_CTS_lambda_old.Rds"))
+saveRDS(racipe, file.path("tests","racipe_CTS_lambda_new.Rds"))
 
 
+# Plot comparison
+racipe_old <- readRDS(file.path("tests","racipe_CTS_lambda_old.Rds"))
+racipe_new <- readRDS(file.path("tests","racipe_CTS_lambda_new.Rds"))
+
+racipe_old <- sracipeNormalize(racipe_old)
+racipe_old <- sracipeNormalize(racipe_old)
+
+assay_old <- assay(racipe_old)
+assay_new <- assay(racipe_new)
+
+
+
+plot(assay_old[1,], assay_old[2,])
+plot(assay_new[1,], assay_new[2,])
+
+
+
+hist(assay_old[2,])
+hist(assay_new[2,])
+
+hist(assay_old[1,])
+hist(assay_new[1,])
 
